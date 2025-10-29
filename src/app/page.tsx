024b275e5447e8c14b53fddd685a1b017b18a4a7
Aurora Meilian Karlsen <Meilian2001@live.no>
2025-10-29 01:19:09 +0100

    Integrate BounceCards component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,34 @@
 import ClientTyping from './components/ClientTyping';
 import Image from 'next/image';
+import BounceCards from './components/BounceCards';
+
+const images = [
+  '/pictureGallery/Skjermbilde 2025-10-28 kl. 23.12.52.jpg',
+  '/pictureGallery/Skjermbilde 2025-10-28 kl. 23.13.06.jpg',
+  '/pictureGallery/Skjermbilde 2025-10-28 kl. 23.13.28.jpg',
+  '/pictureGallery/Skjermbilde 2025-10-28 kl. 23.13.45.jpg',
+  '/pictureGallery/Skjermbilde 2025-10-28 kl. 23.14.29.jpg',
+  '/pictureGallery/Skjermbilde 2025-10-28 kl. 23.14.54.jpg',
+  '/pictureGallery/Skjermbilde 2025-10-28 kl. 23.15.25.jpg',
+  '/pictureGallery/Skjermbilde 2025-10-28 kl. 23.15.41.jpg',
+  '/pictureGallery/Skjermbilde 2025-10-28 kl. 23.14.45.jpg',
+  '/pictureGallery/Skjermbilde 2025-10-28 kl. 23.15.54.jpg',
+  '/pictureGallery/Skjermbilde 2025-10-28 kl. 23.13.55.jpg',
+];
+
+const transformStyles = [
+  'rotate(8deg) translate(-400px)',
+  'rotate(5deg) translate(-300px)',
+  'rotate(-2deg) translate(-200px)',
+  'rotate(3deg) translate(-100px)',
+  'rotate(-10deg) translate(-50px)',
+  'rotate(0deg)',
+  'rotate(2deg) translate(50px)',
+  'rotate(-6deg) translate(100px)',
+  'rotate(5deg) translate(200px)',
+  'rotate(-5deg) translate(300px)',
+  'rotate(-8deg) translate(400px)',
+];
 
 export default function Home() {
   return (
@@ -40,12 +69,26 @@ export default function Home() {
             løsninger. Her kan du lese litt om meg, bakgrunnen min og hva jeg
             jobber med for tiden.
           </p>
-       
-          <p className="mt-4 text-gray-300">
-            Jeg er i det siste blitt glad i å ta bilder, her er noen av mine nyligste knips fra Italia
-          </p>
-        </div>
 
+          <p className="mt-4 text-gray-300">
+            Jeg er i det siste blitt glad i å ta bilder, her er noen av mine
+            nyligste knips fra Italia
+          </p>
+
+          <div className="mt-8 flex justify-center">
+            <BounceCards
+              className="custom-bounceCards"
+              images={images}
+              containerWidth={1200}
+              containerHeight={500}
+              animationDelay={1}
+              animationStagger={0.08}
+              easeType="elastic.out(1, 0.5)"
+              transformStyles={transformStyles}
+              enableHover={true}
+            />
+          </div>
+        </div>
       </section>
 
       <section id="skills" className="min-h-screen px-6 py-24 scroll-mt-20">
