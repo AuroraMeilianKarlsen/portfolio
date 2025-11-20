23b4d458fbb7a22f996ae3f5bdee502ca7c1b7f1
Aurora Meilian Karlsen <Meilian2001@live.no>
2025-11-20 17:00:52 +0100

    Add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,29 @@ import Navbar from './components/Navbar';
 
 export const metadata: Metadata = {
   title: 'Portfolio',
+  icons: {
+    icon: [
+      {
+        url: '/favicon_io/favicon-16x16.png',
+        sizes: '16x16',
+        type: 'image/png',
+      },
+      {
+        url: '/favicon_io/favicon-32x32.png',
+        sizes: '32x32',
+        type: 'image/png',
+      },
+      { url: '/favicon_io/favicon.ico', sizes: 'any' },
+    ],
+    apple: [
+      {
+        url: '/favicon_io/apple-touch-icon.png',
+        sizes: '180x180',
+        type: 'image/png',
+      },
+    ],
+  },
+  manifest: '/favicon_io/site.webmanifest',
 };
 
 export default function RootLayout({
